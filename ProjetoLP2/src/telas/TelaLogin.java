e19890869312e9e8f0a1775273e18ffd40c22146
Agullo <alexandre.gullo@ccc.ufcg.edu.br>
2015-02-06 00:12:34 -0300

    tela de login

---
+++ ProjetoLP2/src/telas/TelaLogin.java
@@ -0,0 +1,66 @@
+package telas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class TelaLogin extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField tfLogin;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.s
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin frame = new TelaLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Login");
+		lblLogin.setBounds(188, 51, 46, 14);
+		contentPane.add(lblLogin);
+		
+		tfLogin = new JTextField();
+		tfLogin.setBounds(168, 76, 86, 20);
+		contentPane.add(tfLogin);
+		tfLogin.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setBounds(188, 119, 46, 14);
+		contentPane.add(lblSenha);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(168, 144, 86, 20);
+		contentPane.add(passwordField);
+	}
+}
