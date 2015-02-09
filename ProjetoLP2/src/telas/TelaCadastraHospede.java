43a36b3e4b7ed79c57e40db96d720fc7a7cc3607
Caio Vitor <caiovitor1996@hotmail.com>
2015-02-09 20:36:23 -0300

    Tela de Check-in

--- ProjetoLP2/src/telas/TelaCadastraHospede.java
+++ ProjetoLP2/src/telas/TelaCadastraHospede.java
@@ -24,7 +24,7 @@ public class TelaCadastraHospede extends JFrame {
 	private JTextField tfCPF;
 	private JTextField tfDataDeNascimento;
 	private JTextField tfEndereco;
-	private JTextField tfNDoCartao;
+	private JTextField tfCartaoDeCredito;
 
 	/**
 	 * Launch the application.
@@ -45,7 +45,7 @@ public class TelaCadastraHospede extends JFrame {
 
 		JLabel lblCpf = new JLabel("CPF:");
 
-		JLabel lblDataDeNascimento = new JLabel("Data de Nascimento:");
+		JLabel lblDataDeNascimento = new JLabel("Nascimento:");
 
 		tfCPF = new JTextField();
 		tfCPF.setColumns(10);
@@ -53,44 +53,37 @@ public class TelaCadastraHospede extends JFrame {
 		tfDataDeNascimento = new JTextField();
 		tfDataDeNascimento.setColumns(10);
 
-		JButton btnNovo = new JButton("Novo");
+		JButton btnFinalizar = new JButton("Finalizar");
 
-		JButton btnGravar = new JButton("Gravar");
-		btnGravar.addActionListener(new ActionListener() {
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-
-		JButton btnExcluir = new JButton("Excluir");
-
-		JButton btnFichaDeHospedes_1 = new JButton("Ficha de hospedes");
-		btnFichaDeHospedes_1.addActionListener(new ActionListener() {
-			@SuppressWarnings("deprecation")
-			public void actionPerformed(ActionEvent e) {
-				TelaFichaHospedes TFH = new TelaFichaHospedes();
-				TFH.show();
-			}
-		});
 		
-		JLabel lblEndereco = new JLabel("Endereco");
+		JLabel lblEndereco = new JLabel("Endereco:");
 		
-		JLabel lblN = new JLabel("N° Do Cartão");
+		JLabel lblN = new JLabel("Cartao de Credito:");
 		
 		tfEndereco = new JTextField();
 		tfEndereco.setColumns(10);
 		
-		tfNDoCartao = new JTextField();
-		tfNDoCartao.setColumns(10);
+		tfCartaoDeCredito = new JTextField();
+		tfCartaoDeCredito.setColumns(10);
 		
 		
 		
-		JComboBox<String> comboBox = new JComboBox();
-		comboBox.addItem("Ré");
-		comboBox.addItem("ré");
-		comboBox.addItem("ré");
-		comboBox.addItem("ré");
+		JComboBox<String> ComBoxQuarto = new JComboBox();
+		ComBoxQuarto.addItem("Ré");
+		ComBoxQuarto.addItem("ré");
+		ComBoxQuarto.addItem("ré");
+		ComBoxQuarto.addItem("ré");
 		
-		String variavel = (String) comboBox.getSelectedItem();
+		String variavel = (String) ComBoxQuarto.getSelectedItem();
+		
+		JLabel lblQuarto = new JLabel("Quarto:");
+		
+		JRadioButton RadioButtonCamaExtra = new JRadioButton("Cama extra");
 
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
@@ -99,40 +92,37 @@ public class TelaCadastraHospede extends JFrame {
 					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblDataDeNascimento)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(tfDataDeNascimento))
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblCpf)
-									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(tfCPF))
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblNome)
-									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(tfNome, GroupLayout.PREFERRED_SIZE, 217, GroupLayout.PREFERRED_SIZE))
-								.addComponent(comboBox, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE))
-							.addGap(57)
+							.addComponent(lblQuarto)
+							.addContainerGap(577, Short.MAX_VALUE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblDataDeNascimento)
+								.addComponent(lblCpf)
+								.addComponent(lblNome))
+							.addGap(18)
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblEndereco)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(tfEndereco, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addComponent(btnFinalizar)
+									.addGap(18)
+									.addComponent(btnCancelar))
 								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblN)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(tfNDoCartao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-							.addContainerGap(75, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(btnNovo)
-							.addPreferredGap(ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
-							.addComponent(btnGravar)
-							.addGap(73)
-							.addComponent(btnExcluir)
-							.addGap(39)
-							.addComponent(btnFichaDeHospedes_1)
-							.addGap(45))))
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(tfNome, GroupLayout.DEFAULT_SIZE, 159, Short.MAX_VALUE)
+										.addComponent(tfCPF, GroupLayout.DEFAULT_SIZE, 159, Short.MAX_VALUE)
+										.addComponent(tfDataDeNascimento, 144, 159, Short.MAX_VALUE)
+										.addComponent(ComBoxQuarto, 0, 159, Short.MAX_VALUE)
+										.addComponent(RadioButtonCamaExtra))
+									.addGap(57)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+										.addGroup(groupLayout.createSequentialGroup()
+											.addComponent(lblEndereco)
+											.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+											.addComponent(tfEndereco, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+										.addGroup(groupLayout.createSequentialGroup()
+											.addComponent(lblN)
+											.addPreferredGap(ComponentPlacement.RELATED)
+											.addComponent(tfCartaoDeCredito, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+									.addGap(142))))))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -140,29 +130,30 @@ public class TelaCadastraHospede extends JFrame {
 					.addGap(50)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblNome)
-						.addComponent(tfNome, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblEndereco)
-						.addComponent(tfEndereco, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(tfEndereco, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(tfNome, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblCpf)
-						.addComponent(tfCPF, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblN)
-						.addComponent(tfNDoCartao, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(tfCartaoDeCredito, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(tfCPF, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblDataDeNascimento)
 						.addComponent(tfDataDeNascimento, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(100)
-					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(180)
+					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnNovo)
-						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-							.addComponent(btnFichaDeHospedes_1)
-							.addComponent(btnExcluir)
-							.addComponent(btnGravar)))
-					.addContainerGap(34, Short.MAX_VALUE))
+						.addComponent(lblQuarto)
+						.addComponent(ComBoxQuarto, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(RadioButtonCamaExtra)
+					.addGap(227)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnFinalizar)
+						.addComponent(btnCancelar))
+					.addContainerGap(21, Short.MAX_VALUE))
 		);
 		getContentPane().setLayout(groupLayout);
 
