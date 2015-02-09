29bd4776134c22499d64922cb33db2d728028aef
RaimundoHeitorMiranda <raimundo.miranda@ccc.ufcg.edu.br>
2015-02-09 19:53:03 -0300

    adicionei atributos a classe hospede e fiz o javadoc.

--- ProjetoLP2/src/hospede/Hospede.java
+++ ProjetoLP2/src/hospede/Hospede.java
@@ -1,4 +1,6 @@
 package hospede;
+
+import java.util.Calendar;
  
 /**
  * Classe que representa uma pessoa que se hospeda no hotel
@@ -8,24 +10,53 @@ package hospede;
 public class Hospede {
  
         private String nome, cpf, endereco;
+        private int numeroDoCartao;
+        private  Calendar dataDeNacimento;
  
         /**
-         *
+         * 
          * @param nome
-         *                      (String) A representacao do nome do hospede
+         * 			(String) A representacao do nome do hospede.
          * @param cpf
-         *                      (String) A representacao do CPF do hospede
+         * 			(String) A representacao do CPF do hospede.
+         * @param dataDeNacimento
+         * 			(String) A representacao da data de nascimento do hospede.
          * @param endereco
-         *                      (String) A representacao do endereco do hospede
+         * 			(String) A representacao do endereco do hospede.
+         * @param NumeroDocartao
+         * 			(String) A representacao do numero do cartao do hospede.
          */
-        public Hospede(String nome, String cpf, String endereco) {
+        public Hospede(String nome, String cpf,Calendar dataDeNacimento, String endereco, int NumeroDocartao) {
                 super();
                 this.nome = nome;
                 this.cpf = cpf;
+                this.dataDeNacimento = dataDeNacimento;
                 this.endereco = endereco;
+                this.dataDeNacimento = dataDeNacimento;
+                this.numeroDoCartao = numeroDoCartao;
         }
  
         /**
+         * 
+         * @return
+         * 		O numero do cartão do hospede.
+         */
+        public int getNumeroDoCartao() {
+			return numeroDoCartao;
+		}
+        
+        /**
+         * 
+         * @return
+         * 		a data de nascimento;
+         */
+
+		public Calendar getDataDeNacimento() {
+			return dataDeNacimento;
+		}
+			
+
+		/**
          * Recupera o nome do hospede
          * @return o nome do hospede
          */
